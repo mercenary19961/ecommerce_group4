0f166c4e0e7b532cbf1b903905362f48f08abefd
Zaid <sabbaghzaid88@gmail.com>
2024-07-31 12:34:54 +0300

    commit

---
+++ includes/header.php
@@ -0,0 +1,76 @@
+<?php
+// session_start();
+include '../config/db_connect.php';
+
+$userLoggedIn = isset($_SESSION['user_id']);
+$cartCount = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My E-commerce Site</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <style>
+        .cart-count {
+            position: absolute;
+            top: -10px;
+            right: -10px;
+            background: red;
+            color: white;
+            border-radius: 50%;
+            padding: 2px 5px;
+            font-size: 12px;
+        }
+    </style>
+</head>
+<body>
+    <svg style="display: none;">
+        <symbol xmlns="http://www.w3.org/2000/svg" id="cart" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </symbol>
+    </svg>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">My E-commerce Site</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="products.php">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                    <?php if ($userLoggedIn): ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="profile.php">Profile</a>
+                        </li>
+                        <li class="nav-item position-relative">
+                            <a class="nav-link" href="cart.php">
+                                <svg class="bi" width="24" height="24"><use xlink:href="#cart"/></svg>
+                                <?php if ($cartCount > 0): ?>
+                                    <span class="cart-count"><?php echo $cartCount; ?></span>
+                                <?php endif; ?>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
+                    <?php else: ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
--- public/products.php
+++ public/products.php
@@ -6,14 +6,38 @@ include '../config/db_connect.php';
 //     header('Location: login.php');
 //     exit();
 // }
+
+$category = isset($_GET['category']) ? $_GET['category'] : 'All';
+$price_min = isset($_GET['price_min']) ? $_GET['price_min'] : 0;
+$price_max = isset($_GET['price_max']) ? $_GET['price_max'] : 3000;
+$in_stock = isset($_GET['in_stock']) ? $_GET['in_stock'] : 'All';
+
+$sql = "SELECT products.*, category.name AS category_name FROM products JOIN category ON products.category_id = category.category_id WHERE 1=1";
+
+if ($category !== 'All') {
+    $sql .= " AND category.category_id = $category";
+}
+if ($price_min !== '') {
+    $sql .= " AND products.price >= $price_min";
+}
+if ($price_max !== '') {
+    $sql .= " AND products.price <= $price_max";
+}
+if ($in_stock !== 'All') {
+    $sql .= " AND products.in_stock = $in_stock";
+}
+
+$result = $conn->query($sql);
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Store</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
     <script src="../js/jquery-1.11.0.min.js"></script>
     <script src="../js/bootstrap.bundle.min.js"></script>
 </head>
@@ -26,77 +50,45 @@ include '../config/db_connect.php';
                 <div class="col-md-3">
                     <label for="category">Category</label>
                     <select name="category" id="category" class="form-control">
-                        <option value="">All</option>
-                        <option value="Mobile">Mobile</option>
-                        <option value="Watch">Watch</option>
-                        <option value="Console">Console</option>
+                        <option value="All">All</option>
+                        <option value="1">Mobile</option>
+                        <option value="2">Watch</option>
+                        <option value="3">Console</option>
                     </select>
                 </div>
                 <div class="col-md-3">
                     <label for="price_min">Price (Min)</label>
-                    <input type="number" name="price_min" id="price_min" class="form-control" placeholder="0">
+                    <input type="number" name="price_min" id="price_min" class="form-control" placeholder="0" value="<?= $price_min ?>">
                 </div>
                 <div class="col-md-3">
                     <label for="price_max">Price (Max)</label>
-                    <input type="number" name="price_max" id="price_max" class="form-control" placeholder="3000">
+                    <input type="number" name="price_max" id="price_max" class="form-control" placeholder="3000" value="<?= $price_max ?>">
                 </div>
                 <div class="col-md-3">
                     <label for="in_stock">In Stock</label>
                     <select name="in_stock" id="in_stock" class="form-control">
-                        <option value="">All</option>
+                        <option value="All">All</option>
                         <option value="1">In Stock</option>
                         <option value="0">Out of Stock</option>
                     </select>
                 </div>
             </div>
-            <div class="row mt-3">
-                <div class="col-md-12">
-                    <button type="submit" class="btn btn-primary">Filter</button>
-                </div>
-            </div>
+            <button type="submit" class="btn btn-primary mt-3">Filter</button>
         </form>
 
         <div class="row">
-            <?php
-            $category = isset($_GET['category']) ? $_GET['category'] : '';
-            $price_min = isset($_GET['price_min']) ? $_GET['price_min'] : 0;
-            $price_max = isset($_GET['price_max']) ? $_GET['price_max'] : 3000;
-            $in_stock = isset($_GET['in_stock']) ? $_GET['in_stock'] : '';
-
-            $sql = "SELECT * FROM products WHERE 1=1";
-
-            if ($category) {
-                $sql .= " AND category = '$category'";
-            }
-            if ($price_min) {
-                $sql .= " AND price >= $price_min";
-            }
-            if ($price_max) {
-                $sql .= " AND price <= $price_max";
-            }
-            if ($in_stock !== '') {
-                $sql .= " AND in_stock = $in_stock";
-            }
-
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<div class='col-md-4'>
-                            <div class='card mb-4'>
-                                <img src='" . $row['image'] . "' class='card-img-top' alt='" . $row['name'] . "'>
-                                <div class='card-body'>
-                                    <h5 class='card-title'>" . $row['name'] . "</h5>
-                                    <p class='card-text'>$" . $row['price'] . "</p>
-                                    <a href='#' class='btn btn-primary'>Add to Cart</a>
-                                </div>
-                            </div>
-                          </div>";
-                }
-            } else {
-                echo "<p>No products found</p>";
-            }
-            ?>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../images/<?= $row['image'] ?>" class="card-img-top" alt="<?= $row['name'] ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $row['name'] ?></h5>
+                            <p class="card-text">$<?= $row['price'] ?></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
         </div>
     </main>
     <?php include '../includes/footer.php'; ?>
