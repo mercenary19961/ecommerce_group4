f10ff6aeb09e8acb74b72199be7463fe8717a5f9
Basharoqilan <basharoqilan2@gmail.com>
2024-07-31 12:30:43 +0300

    tet

--- img.php
+++ img.php
@@ -11,7 +11,7 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
-
+//sdjfndsjfbdsjb
 // القيم التي سيتم إدخالها
 $product_id = '1';
 $name = 'Example Product';
